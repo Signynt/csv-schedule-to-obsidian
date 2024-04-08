cfe8fcec5c06dfb21487e983ec99733d11ef736c
Signynt <67801159+Signynt@users.noreply.github.com>
2024-04-09 01:25:00 +0200

    Updated script to improve terminal output to show missing loctions, and added dictionary entries for semester 8

--- csv-schedule-to-obsidian.py
+++ csv-schedule-to-obsidian.py
@@ -41,6 +41,8 @@ class ImportCSV:
 
         # dictionary for LV_ART
         dictionary_art = {"VO": "Vorlesung", "SE": "Seminar", "PR": "Praktikum", "FA": "Prüfung"}
+        
+        duplicate_orts = []
 
         with open(self.csvFiles[0], mode='r', encoding='utf-8') as csv_file:
             csv_reader = csv.DictReader(csv_file)
@@ -55,15 +57,14 @@ class ImportCSV:
 
                 # splits lists of professors up by '/'
                 prof_list = line['VORTRAGENDER_KONTAKTPERSON'].split("/")
-                seperator = ", "
                 
-                # function that reformats names from 'Lastname, Firstname; Doktortitel' to '"[[Firstname Lastname]]"'
+                # function that reformats names from 'Lastname, Firstname; Doktortitel' to '" - [[Firstname Lastname]]"'
                 def reformat_names(input):
                     return re.sub(r"\s?((.*),)\s?(.[^;]*)(.*)", r'\n  - "[[\3 \2]]"', input)
 
                 # reformats name lists and combines them again
                 reformated_prof_list = list(map(reformat_names, prof_list))
-                professors = seperator.join(reformated_prof_list)
+                professors = ''.join(reformated_prof_list)
 
                 # replace LV_ART entries
                 for entry in dictionary_art:
@@ -73,24 +74,38 @@ class ImportCSV:
                     line['TITEL'] = line['TITEL'].replace(entry, dictionary_titel[entry])
 
                 for entry in dictionary_ort:
-                    line['ORT'] = line['ORT'].replace(entry, f"\"[[{dictionary_ort[entry]}]]\"")
+                    if dictionary_ort[entry]:
+                        line['ORT'] = line['ORT'].replace(entry, f"\"[[{dictionary_ort[entry]}]]\"")
+                    else:
+                        line['ORT'] = line['ORT'].replace(entry, "")
+
+                if line['ORT'] == line['ORT']:
+                    duplicate_orts.append(line['ORT'])
 
                 line['ORT'] = re.sub(r"(.*siehe.*)", r"",line['ORT'])
 
                 content = f"---\ntitle: {line['TITEL']}\nallDay: false\nstartTime: {line['VON']}\nendTime: {line['BIS']}\ndate: {line['DATUM']}\nclass-type: {line['LV_ART']}\nlocation: {line['ORT']}\nprofessor: {professors}\ntopic: {line['ANMERKUNG']}\nreviewed: no\n---\n\n"
 
                 fileName = f"./output/{line['DATUM']} {line['TITEL']}.md"
+                counter = 2
+                while os.path.exists(fileName):
+                    fileName = f"./output/{line['DATUM']} {line['TITEL']} {counter}.md"
+                    counter += 1
 
-                fileNameSecond = f"./output/{line['DATUM']} {line['TITEL']} 2.md"
-
-                if not os.path.exists(fileName):
-                    with open (fileName, "w", encoding='utf-8') as f:
-                        f.write(content)
-                elif os.path.exists(fileName):
-                    with open (fileNameSecond, "w", encoding='utf-8') as f:
-                        f.write(content)
+                with open(fileName, "w", encoding='utf-8') as f:
+                    f.write(content)
 
             # content_replaced = [substring.replace("VO", "Vorlesung") for substring in content]
             # print(content[2] + "\n" + fileName)
+                
+        # Filter the list for duplicates
+        unique_orts = list(set(duplicate_orts))
+
+        # Filter the list for entries already in the dictionary
+        unique_orts = [ort for ort in unique_orts if ort.strip('"[[').strip(']]"') not in dictionary_ort.values()]
+
+        unique_orts = '\n'.join(unique_orts)
+
+        print(unique_orts)
 
 startScript = ImportCSV()
